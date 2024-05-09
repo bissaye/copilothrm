a3447177f7e11cb8f62643eeafbb312c180d4f99
bissaye <bissaye.com@gmail.com>
2024-05-09 12:47:27 +0100

    feat: ajout d'une couleur par défaut dans les configs tailwind de l'app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6366F1',
+      },
+    },
   },
   plugins: [],
 }
