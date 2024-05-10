d137e03946833a6bc6e60f82f989a8dfaf4ad336
olive.mengolo <mengolocharlie@gmail.com>
2024-05-10 19:05:02 +0100

    refactor: déplacement de landingPage de 'page/' vers 'page/landingPage/' et ajout de son fichier style.css

--- src/views/pages/index.ts
+++ src/views/pages/index.ts
@@ -1,7 +1,7 @@
 import { HomePage } from "./homePage";
 import { SignInPage } from "./signInPage";
 import { SignUpPage } from "./signUpPage";
-import { LandingPage } from "./landingPage";
+import { LandingPage } from "./landingPage/landingPage";
 import { NotFoundPage } from "./notFoundPage";
 
 
