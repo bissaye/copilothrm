4192fb6fdbe9b7b363b61690d1a54ac6ef9785a7
bissaye <bissaye.com@gmail.com>
2024-05-16 10:22:37 +0100

    feat: ajout d'un props size pour la définir un taille pour le stepper et mis en forme du text des boutons

--- src/views/components/common/stepper/stepper.tsx
+++ src/views/components/common/stepper/stepper.tsx
@@ -4,20 +4,27 @@ import { PuceButton } from '../../ui'
 
 interface StepperProps {
     stepList : string [],
-    currentStep : number
+    currentStep : number,
+    size? : number 
 }
 
 
 export const Stepper : React.FC<StepperProps> = (props : StepperProps) => {
 
-    const { stepList, currentStep } = props;
+    const { stepList, currentStep, size} = props;
+
+    let width = 220;
+
+    if(size){
+        width = size;
+    }
 
     useEffect( () => {
         console.log(`next step ${currentStep}`)
     }, [currentStep])
 
     return <Fragment>
-        <div className='flex flex-row justify-between mb-4'>
+        <div className='flex flex-row justify-between mb-4 ' style={{width: `${width}px`}}>
             {
                 stepList.map((stepTitle, index ) => (
                     <div className='flex flex-row items-center' key={index}>
@@ -26,11 +33,14 @@ export const Stepper : React.FC<StepperProps> = (props : StepperProps) => {
                                 text = {(index +1).toString()}
                                 bgPrimary={currentStep > index ? 200 : 50}
                                 className='stepper-puce-transition-colors'
+                                width={width/4}
                             />
                             <p 
                                 className={`absolute capitalize text-center ${currentStep > index ? "font-bold" : "font-semibold text-primary-50"} `}
                                 style={{
-                                    bottom: `-${stepTitle.length}%`
+                                    bottom: `-${stepTitle.length}%`,
+                                    transform: 'translateY(100%)',
+                                    width: '100%'
                                 }}
                             >
                                 {stepTitle}
