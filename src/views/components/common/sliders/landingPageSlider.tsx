c16543d09f9c2d05d05f48474905e4f585466a84
bissaye <bissaye.com@gmail.com>
2024-05-16 08:22:11 +0100

    fix: retrait des doubles classes tailwind, retrait des manipulation de dom et utilisation des states

--- src/views/components/common/sliders/landingPageSlider.tsx
+++ src/views/components/common/sliders/landingPageSlider.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleChevronLeft, faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { sliderImagesBackground } from "../../../../assets/images";
@@ -12,13 +12,14 @@ export const LandingPageSlider: React.FC<LandingPageSliderProps> = () => {
 
     const imagesList = Object.values(sliderImagesBackground);
 
-    let indexValue = 0;
+    const [indexValue, setIndexValue] = useState<number>(0);
+    const [slideValue, setSlideValue] = useState<number>(indexValue * (-100));
 
     const showImg = (e: number) => {
         if (e >= imagesList.length)
-            indexValue = 0;
+            setIndexValue(0);
         if (e < 0)
-            indexValue = imagesList.length - 1;
+            setIndexValue(imagesList.length - 1);
 
         const img0 = document.getElementById("img0");
         const percentage = (indexValue) * (-100) + "%";
@@ -41,28 +42,45 @@ export const LandingPageSlider: React.FC<LandingPageSliderProps> = () => {
 
     }
 
-    showImg(indexValue);
 
-    const sideSlide = (e: number) => {
-        showImg(indexValue += e);
-    }
+    const sideSlide = (direction: number) => {
+        setIndexValue((prevIndex) => {
+            let newIndex = prevIndex + direction;
+            if (newIndex < 0) {
+                return imagesList.length - 1;
+            } else if (newIndex >= imagesList.length) {
+                return 0;
+            }
+            return newIndex;
+        });
+    };
 
-    const btnSlide = (e: number) => {
-        indexValue = e;
-        showImg(indexValue)
-    }
-
-    const autoPlay = () => {
-        showImg(indexValue += 1);
-    }
+    const btnSlide = (index: number) => {
+        setIndexValue(index);
+    };
 
     useEffect(() => {
-        var timer = setInterval(autoPlay, 3000);
+        var timer = setInterval( () => {
+            setIndexValue((prevIndex) => {
+                let newIndex = prevIndex + 1;
+                if (newIndex < 0) {
+                    return imagesList.length - 1;
+                } else if (newIndex >= imagesList.length) {
+                    return 0;
+                }
+                return newIndex;
+            });
+            }   
+            , 3000);
 
         return () => {
             clearInterval(timer);
         }
-    });
+    }, []);
+
+    useEffect(()=>{
+        setSlideValue(-100 * indexValue);
+    }, [indexValue])
 
     return (
         <Fragment>
@@ -75,16 +93,25 @@ export const LandingPageSlider: React.FC<LandingPageSliderProps> = () => {
                 <div className="flex flex-col w-full h-full relative">
                     <div className="flex flex-row overflow-hidden w-full h-full">
                         {imagesList.map((img, index) => {
-                            let id = "img" + index;
-                            return <img key={index} src={img} id={id} className='sliderImage w-full h-full transition ease-in-out duration-300' />
+                            const id = "img" + index;
+                            return <img 
+                                        key={index} 
+                                        src={img} 
+                                        id={id} 
+                                        className='sliderImage w-full h-full transition ease-in-out duration-300' 
+                                        style={index == 0  ? {
+                                            marginLeft: `${slideValue}%`,
+                                            transition: '1s'
+                                        }: {}}
+                                    />
                         })}
                     </div>
                     <div className="mb-3 h-0 flex flex-row items-center justify-center gap-4 absolute bottom-[5px] left-[46%]">
                         {imagesList.map((_, index) => {
-                            let id = "button-" + index;
+                            const id = "button-" + index;
                             return <button
                                 key={index}
-                                className="slide-buttons cursor-pointer w-[15px] h-[15px] rounded-full border border-2 border-white"
+                                className={`slide-buttons cursor-pointer w-[15px] h-[15px] rounded-full border-2 border-white transition-all ${index === indexValue ? 'btn-active' : ''} `}
                                 id={id}
                                 onClick={() => btnSlide(index)}
                             ></button>
