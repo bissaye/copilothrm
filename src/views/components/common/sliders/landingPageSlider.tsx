59a3ba40724f81c344e91cf173c679d75c152763
bissaye <bissaye.com@gmail.com>
2024-05-16 01:08:43 +0100

    refactor: mise à jour du format du document

--- src/views/components/common/sliders/landingPageSlider.tsx
+++ src/views/components/common/sliders/landingPageSlider.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect} from "react";
+import React, { Fragment, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleChevronLeft, faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { sliderImagesBackground } from "../../../../assets/images";
@@ -15,32 +15,32 @@ export const LandingPageSlider: React.FC<LandingPageSliderProps> = () => {
     let indexValue = 0;
 
     const showImg = (e: number) => {
-        if(e >= images.length) 
+        if (e >= images.length)
             indexValue = 0;
-        if(e < 0) 
+        if (e < 0)
             indexValue = images.length - 1;
-        
+
         const img0 = document.getElementById("img0");
-        const percentage = (indexValue)*(-100) + "%";
-        if(img0 != null ) {
+        const percentage = (indexValue) * (-100) + "%";
+        if (img0 != null) {
             img0!.style.transition = `1s`;
             img0!.style.marginLeft = `${percentage}`;
         };
 
         var slideButtons = document.getElementsByClassName("slide-buttons");
-        for(var i = 0; i < slideButtons.length; i++){
-            if(slideButtons[i].classList.contains("btn-active"))
+        for (var i = 0; i < slideButtons.length; i++) {
+            if (slideButtons[i].classList.contains("btn-active"))
                 slideButtons[i].classList.remove("btn-active")
         }
         var buttonId = "button-" + indexValue;
         const btn = document.getElementById(buttonId);
-        if(btn != null ) {
+        if (btn != null) {
             btn!.style.transition = '0.7s';
             btn!.classList.add("btn-active");
         };
 
     }
-    
+
     showImg(indexValue);
 
     const sideSlide = (e: number) => {
@@ -64,35 +64,36 @@ export const LandingPageSlider: React.FC<LandingPageSliderProps> = () => {
         }
     });
 
-    return(
+    return (
         <Fragment>
             <div className=" h-full w-full bg-slate-200 flex flex-row justify-between items-center">
-                <FontAwesomeIcon 
-                    icon={faCircleChevronLeft} 
+                <FontAwesomeIcon
+                    icon={faCircleChevronLeft}
                     className=" ml-10 text-6xl text-white/60 cursor-pointer hover:text-white/90 transition ease-in-out duration-300 absolute z-10 left-[10px]"
-                    onClick={() => sideSlide(-1)} />
-                    <div className="flex flex-col w-full h-full relative">
-                        <div className="flex flex-row overflow-hidden w-full h-full">
-                            {images.map((img, index) => {
-                                let id = "img" + index;
-                                return <img key={index} src={img} id={id} className='sliderImage w-full h-full transition ease-in-out duration-300'/>
-                            })}
-                        </div>  
-                        <div className="mb-3 h-0 flex flex-row items-center justify-center gap-4 absolute bottom-[5px] left-[46%]">
-                            {images.map((_, index) => {
-                                let id = "button-" + index;
-                                return <button
+                    onClick={() => sideSlide(-1)}
+                />
+                <div className="flex flex-col w-full h-full relative">
+                    <div className="flex flex-row overflow-hidden w-full h-full">
+                        {images.map((img, index) => {
+                            let id = "img" + index;
+                            return <img key={index} src={img} id={id} className='sliderImage w-full h-full transition ease-in-out duration-300' />
+                        })}
+                    </div>
+                    <div className="mb-3 h-0 flex flex-row items-center justify-center gap-4 absolute bottom-[5px] left-[46%]">
+                        {images.map((_, index) => {
+                            let id = "button-" + index;
+                            return <button
                                 key={index}
                                 className="slide-buttons cursor-pointer w-[15px] h-[15px] rounded-full border border-2 border-white"
                                 id={id}
                                 onClick={() => btnSlide(index)}
-                                ></button>
-                            })}
-                        </div>
+                            ></button>
+                        })}
                     </div>
-                <FontAwesomeIcon 
-                    icon={faCircleChevronRight} 
-                    className=" mr-10 text-6xl text-white/60 cursor-pointer hover:text-white/90 transition ease-in-out duration-300 absolute z-10 right-[10px]" 
+                </div>
+                <FontAwesomeIcon
+                    icon={faCircleChevronRight}
+                    className=" mr-10 text-6xl text-white/60 cursor-pointer hover:text-white/90 transition ease-in-out duration-300 absolute z-10 right-[10px]"
                     onClick={() => sideSlide(1)} />
             </div>
         </Fragment>
