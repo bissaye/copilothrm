d6239c26bd6c7206219c36cf2fbd26dad6b55ef7
bissaye <bissaye.com@gmail.com>
2024-05-27 13:00:27 +0100

    feat: export du nouveau store usePageStore

--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -1,7 +1,8 @@
 import { useAuthStore } from "./auth/authStore";
 import { useLangStore } from "./lang/langStore";
-
+import { usePageStore } from "./page/pageStore";
 export {
     useLangStore,
-    useAuthStore
+    useAuthStore,
+    usePageStore
 }
