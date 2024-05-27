8e165c2409f6195ff86da464038671c6c471707c
bissaye <bissaye.com@gmail.com>
2024-05-27 12:30:40 +0100

    feat mise à jour du design de la landing page

--- src/views/pages/landingPage/landingPage.tsx
+++ src/views/pages/landingPage/landingPage.tsx
@@ -1,45 +1,113 @@
-import React, { Fragment } from 'react';
+import React, { Fragment} from 'react';
 import { useIntl } from 'react-intl';
-import "./style.css";
-import { LandingPageSlider } from '../../components/common/sliders';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBuilding, faCheck, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { landingPageImage, avatars } from '../../../assets/images';
+import { DefaultButton } from '../../components/ui';
+import { KeyFunction } from '../../components/common';
+import { faBuilding, faHandHoldingDollar, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { UserNotice } from '../../components/common';
+import { FooterLandingPage } from '../../components/common';
+
 
 export const LandingPage : React.FC = () => {
 
     const {formatMessage} = useIntl();
 
     return <Fragment>
-        <div className='landingPageContent w-full flex flex-col bg-primary-50'>
-            <div className=' h-[30rem] capitalize text-4xl font-bold text-white bg-white flex flex-col justify-center text-center tracking-wide'>
-                <LandingPageSlider />
+        <div className=' w-full h-full flex flex-col justify-start items-start p-0 bg-white lg:overflow-x-hidden'>
+
+            <div className='w-full flex flex-col justify-center font-body text-black text-center h-20 bg-primary-150'>
+                {formatMessage({id:"landing_page_header_text"})}
             </div>
-            <div className=' h-96 px-48 capitalize text-4xl font-bold text-primary bg-white flex flex-row justify-between items-center tracking-wide'>
-                <div className='flex flex-col justify-center'>
-                    <FontAwesomeIcon icon={faUserCircle} className="text-8xl text-black mb-3" />
-                    <span className='flex flex-col justify-center items-center'>
-                        <p className="text-primary-100">{"2750"}</p>
-                        <p className="font-thin text-black text-[25px]">{formatMessage({id: "user"})}</p>
-                    </span>
+
+            <div className='w-full h-auto flex flex-row justify-around items-center mt-20'>
+                <div className='flex flex-col'>
+                    <h1 className=' font-heading font-bold text-secondary-800  text-t10-2'>Copilot HRM</h1>
+                    <p className='font-heading font-bold text-secondary-800 text-t10 text-left max-w-[400px] h-auto'>
+                        {formatMessage({id: "your_solution"})}
+                    </p>
+                    <p className='font-heading text-gray-800 text-t5 text-justify max-w-[500px] h-auto mt-5'>
+                        {formatMessage({id: "capital_gain"})}
+                    </p>
+                    <div className='w-full flex flex-row justify-between items-center mt-5'>
+                        <DefaultButton
+                            type='primary'
+                            text={formatMessage({id:"begin_now"})}
+                            bgWhite={false}
+                            width={235}
+                            height={52}
+                            paddingX={20}
+                            className='capitalize'
+                        />
+                        <button className=' bg-primary-150 rounded-xl text-primary w-52 h-14 text-t5 capitalize'>
+                            {formatMessage({id:"request_demo"})}
+                        </button>
+                    </div>
                 </div>
-                <div className='flex flex-col justify-center'>
-                    <FontAwesomeIcon icon={faBuilding} className="text-8xl text-black mb-3" />
-                    <span className='flex flex-col justify-center items-center'>
-                        <p className="text-primary-100">{"2750"}</p>
-                        <p className="font-thin text-black text-[25px]">{formatMessage({id: "entreprise"})}</p>
-                    </span>
-                </div>
-                <div className='flex flex-col justify-center'>
-                    <FontAwesomeIcon icon={faCheck} className="text-8xl text-black mb-3" />
-                    <span className='flex flex-col justify-center items-center'>
-                        <p className="text-primary-100">{"2750"}</p>
-                        <p className="font-thin text-black text-[25px]">{formatMessage({id: "positive_reviews"})}</p>
-                    </span>
-                </div>
+                <img src={landingPageImage.img1} className=''/>
             </div>
-            <div className=' h-60 capitalize text-4xl font-bold text-white bg-primary-300 flex flex-col justify-center text-center tracking-wide'>
-                {"Footer"}
+
+            <h1 className='font-body font-bold text-secondary-800 text-t8 text-center w-full capitalize my-10'>
+                {formatMessage({id: "key_functions"})}
+            </h1>
+
+            <div className='w-full h-auto flex flex-row justify-around items-center'>
+                <KeyFunction
+                    icon={faBuilding}
+                    colorType='primary'
+                    title={formatMessage({id:"org_management"})}
+                    text={formatMessage({id:"org_management_text"})}
+                />
+
+                <KeyFunction
+                    icon={faHandHoldingDollar}
+                    colorType='secondary'
+                    title={formatMessage({id:"payroll_management"})}
+                    text={formatMessage({id:"payroll_management_text"})}
+                />
+
+                <KeyFunction
+                    icon={faUsers}
+                    colorType='green'
+                    title={formatMessage({id:"human_ressources_management"})}
+                    text={formatMessage({id:"human_ressources_management_ext"})}
+                />
             </div>
+
+            <h1 className='font-body font-bold text-secondary-800 text-t8 text-center w-full capitalize my-10'>
+                {formatMessage({id: "notice"})}
+            </h1>
+
+            <div className='w-full flex gap-3 mx-8 pb-2 overflow-x-scroll mb-14'>
+                <UserNotice
+                    avatar= {avatars.avatarLandingPage}
+                    name='Jennifer king'
+                    date={ new Date('1/1/2024')}
+                    rate={3}
+                    comment='Nulla laboris fugiat fugiat minim minim excepteur eiusmod quis. Laborum est minim id cillum nostrud cillum consectetur 😍😍😍'
+                />
+                <UserNotice
+                    avatar= {avatars.avatarLandingPage}
+                    name='Jennifer king'
+                    date={ new Date('1/1/2024')}
+                    rate={3}
+                    comment='Nulla laboris fugiat fugiat minim minim excepteur eiusmod quis. Laborum est minim id cillum nostrud cillum consectetur 😍😍😍'
+                />
+                             <UserNotice
+                    avatar= {avatars.avatarLandingPage}
+                    name='Jennifer king'
+                    date={ new Date('1/1/2024')}
+                    rate={3}
+                    comment='Nulla laboris fugiat fugiat minim minim excepteur eiusmod quis. Laborum est minim id cillum nostrud cillum consectetur 😍😍😍'
+                />
+                                <UserNotice
+                    avatar= {avatars.avatarLandingPage}
+                    name='Jennifer king'
+                    date={ new Date('1/1/2024')}
+                    rate={3}
+                    comment='Nulla laboris fugiat fugiat minim minim excepteur eiusmod quis. Laborum est minim id cillum nostrud cillum consectetur 😍😍😍'
+                />
+            </div>
+            <FooterLandingPage/>
         </div>
     </Fragment>
 }
