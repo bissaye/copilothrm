2f6fbe3a623fb8b7d1fb310fe46a9532a4d1ed22
olive.mengolo <mengolocharlie@gmail.com>
2024-05-28 12:52:19 +0100

    fix : modification de l'état isLogged

--- src/services/store/auth/authStore.ts
+++ src/services/store/auth/authStore.ts
@@ -2,14 +2,16 @@ import { create } from "zustand";
 import { AuthStore } from "../../../utils/interfaces/type/store";
 
 
-export const useAuthStore = create<AuthStore>(() =>({
+export const useAuthStore = create<AuthStore>((set) =>({
     isLogged : localStorage.getItem("isLogged") === "1",
     signIn : async ()  =>{
         localStorage.setItem("isLogged", "1");
+        set({isLogged: true});
         return true;
     },
     signOut: async ()  =>{
         localStorage.setItem("isLogged", "0");
+        set({isLogged: false});
         return false
     },
     initAuth: (): void => {
