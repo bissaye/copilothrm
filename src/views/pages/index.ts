2919354b7d72bd52c8d7abf55958b23d39845e54
bissaye <bissaye.com@gmail.com>
2024-05-30 01:32:48 +0100

    feat: correction de l'import de la page ChooseOrg

--- src/views/pages/index.ts
+++ src/views/pages/index.ts
@@ -3,7 +3,7 @@ import { SignInPage } from "./signInPage";
 import { SignUpPage } from "./signUpPage";
 import { LandingPage } from "./landingPage/landingPage";
 import { NotFoundPage } from "./notFoundPage";
-import { ChooseOrg } from "./ChooseOrg";
+import { ChooseOrg } from "./ChooseOrgPage";
 
 
 export {
