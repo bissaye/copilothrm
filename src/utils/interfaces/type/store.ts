5476cc7bb6b5a1eeb19a0b39f520004bf35fe031
olive.mengolo <mengolocharlie@gmail.com>
2024-05-30 12:29:43 +0100

    feat: ajout du type de store pour la création de compte

--- src/utils/interfaces/type/store.ts
+++ src/utils/interfaces/type/store.ts
@@ -1,3 +1,5 @@
+import { UserSignupData } from "../DTO/request";
+
 export type AuthStore = {
     isLogged: boolean;
     signIn: () => Promise<boolean>;
@@ -8,4 +10,6 @@ export type AuthStore = {
 export type SignupStore = {
   signupStep: 1 | 2 | 3 | 4;
   setSignupStep: (step: 1 | 2 | 3 | 4) => void;
+  userData: UserSignupData;
+  setUserData: (values: UserSignupData) => void
 }
