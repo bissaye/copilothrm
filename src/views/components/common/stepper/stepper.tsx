b3663f158aeaa8e6e155e2062ee0a926784dcece
bissaye <bissaye.com@gmail.com>
2024-05-30 10:11:12 +0100

    feat: mise àa jour du stepper en fonction du nouveau thème

--- src/views/components/common/stepper/stepper.tsx
+++ src/views/components/common/stepper/stepper.tsx
@@ -1,52 +1,51 @@
-import React, { Fragment, useEffect } from 'react';
-import { PuceButton } from '../../ui'
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import './style.css'
 
 
 interface StepperProps {
-    stepList : string [],
+    steps: number,
     currentStep : number,
-    size? : number 
 }
 
 
 export const Stepper : React.FC<StepperProps> = (props : StepperProps) => {
 
-    const { stepList, currentStep, size} = props;
-
-    let width = 220;
-
-    if(size){
-        width = size;
-    }
+    const { steps, currentStep} = props;
+    const [upperStage, setUpperStage] = useState<boolean>(true);
+    const prevStepRef = useRef<number>(currentStep);
 
     useEffect( () => {
-        console.log(`next step ${currentStep}`)
+        if (prevStepRef.current !== currentStep) {
+            if (currentStep > prevStepRef.current) {
+                setUpperStage(true);
+            }
+            else{
+                setUpperStage(false);
+            }
+            prevStepRef.current = currentStep;
+        }
     }, [currentStep])
 
     return <Fragment>
-        <div className='flex flex-row justify-between mb-4 ' style={{width: `${width}px`}}>
+        <div className='flex flex-row justify-between mb-4 '>
             {
-                stepList.map((stepTitle, index ) => (
+                Array.from( {length:steps}, (_, index ) => (
                     <div className='flex flex-row items-center' key={index}>
-                        <div className='flex flex-col justify-start items-center relative'>
-                            <PuceButton
-                                text = {(index +1).toString()}
-                                bgPrimary={currentStep > index ? 200 : 50}
-                                className='stepper-puce-transition-colors'
-                                width={width/4}
-                            />
-                            <p 
-                                className={`absolute capitalize text-center ${currentStep > index ? "font-bold" : "font-semibold text-primary-50"} `}
-                                style={{
-                                    bottom: `-${stepTitle.length}%`,
-                                    transform: 'translateY(100%)',
-                                    width: '100%'
-                                }}
-                            >
-                                {stepTitle}
-                            </p>
+                        <div className={` flex flex-col justify-center w-s5 h-s5 rounded-full ${ currentStep === index+1 ? " transition ease-in duration-[1200ms] border-4 border-primary" : currentStep > index+1 ? "border-none bg-primary" : "border-2 border-gray-650"}`}>
+                            {
+                                currentStep > index+1 &&
+                                <FontAwesomeIcon icon={faCheck} className=' text-t2 text-white text-center'/>
+                            }
                         </div>
-                        {index !== stepList.length - 1 && <hr className={`w-12 border-t-2 ${currentStep > (index +1) ? "border-primary-200" : "border-primary-50"} my-1 stepper-bar-transition-colors`} />}
+                        {
+                            index !== steps-1 && 
+                            <div className={` relative w-[78px] h-s2 border-t-2 overflow-hidden ${ currentStep - (index+1) === 1 && upperStage ? "bg-gray-650" : currentStep > index+1 ? "bg-primary" : "bg-gray-650 my-1" } `}> 
+                                <div className={` absolute w-full h-full bg-primary ${currentStep - (index+1) === 1 && upperStage ? "stepper-transition-bar-forward" : currentStep === (index+1) && !upperStage ? "stepper-transition-bar-back" : "hidden" }`}>
+                                </div>
+                            </div>
+                        }
                     </div>
                 ))
             }
