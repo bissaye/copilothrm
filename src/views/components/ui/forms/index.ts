924a65f60e5ea128cdf3f457fd01075c27dbb7b1
bissaye <bissaye.com@gmail.com>
2024-06-01 18:53:38 +0100

    feat: export de tous les nouveaux composants créés

--- src/views/components/ui/forms/index.ts
+++ src/views/components/ui/forms/index.ts
@@ -2,10 +2,14 @@ import { InputText } from "./InputText";
 import { InputPassword } from "./InputPassword";
 import { InputSelect } from "./InputSelect";
 import { InputDate } from "./InputDate";
+import { InputDigit } from "./InputDigit";
+import { InputFile } from "./InputFile";
 
 export {
     InputText,
     InputPassword,
     InputSelect,
-    InputDate
+    InputDate,
+    InputDigit,
+    InputFile
 }
