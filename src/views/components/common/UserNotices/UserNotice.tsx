c32e3b20b6785aa2b77b85feaced2a1d22c5bb82
olive.mengolo <mengolocharlie@gmail.com>
2024-06-03 18:08:35 +0100

    feat: ajout du responsive sur le footer, la landing, et la page de login

--- src/views/components/common/UserNotices/UserNotice.tsx
+++ src/views/components/common/UserNotices/UserNotice.tsx
@@ -15,9 +15,10 @@ export const UserNotice : React.FC<UserNoticeProps> =  (props: UserNoticeProps) 
     const {avatar, name, date, rate, comment} = props;
 
     return <Fragment>
-        <div className=' lg:min-w-[574px] max-w-[574px] h-[119px] p-2 border border-gray-400 bg-white shadow-xs flex flex-col justify-start items-start'>
+        <div className=' lg:min-w-[574px] max-w-[574px] w-[350px] max-h-[250px] md:h-[119px] p-2 border border-gray-400 bg-white shadow-xs flex flex-col justify-start items-start gap-6'>
            <div className='flex flex-row justify-evenly gap-4 items-start h-[40px]'>
                 <img src={avatar} className='w-9 h-9 rounded-full'/>
+                <div className="flex flex-col md:flex-row">
                 <div className='h-full flex flex-col justify-between items-start'>
                     <h1 className=' font-body font-bold text-black text-t3 capitalize'>
                         {name}
@@ -40,6 +41,8 @@ export const UserNotice : React.FC<UserNoticeProps> =  (props: UserNoticeProps) 
                 <p className=' text-gray-700 text-t2 font-body'>
                     il y a {Math.round((new Date().getTime()- date.getTime())/(1000*3600*24))} jour(s)
                 </p>
+
+                </div>
            </div>
            <p className='w-full text-black text-t3'>
                 {
