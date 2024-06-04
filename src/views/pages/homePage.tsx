3d864da15327be1a935cffa45d947e8d13c30bd0
olive.mengolo <mengolocharlie@gmail.com>
2024-06-04 14:50:16 +0100

    feat: mise à jour de la home page

--- src/views/pages/homePage.tsx
+++ src/views/pages/homePage.tsx
@@ -5,6 +5,8 @@ export const HomePage : React.FC = () => {
     const {formatMessage} = useIntl();
 
     return <Fragment>
-        {formatMessage({id:"home_Page"})}
+        <div className=' w-full h-full flex flex-col justify-start items-start p-0 bg-white lg:overflow-x-hidden'>
+            
+        </div>
     </Fragment>
 }
