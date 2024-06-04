b09f822706427b78adbb3d39b90d2aa52d0f9862
olive.mengolo <mengolocharlie@gmail.com>
2024-06-04 14:53:30 +0100

    feat: ajout des pagesIds des menus de la Sidebar

--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -3,6 +3,13 @@ export const pageIds  = {
     SignInPage : "SignInPage",
     SignUpPage : "SignUpPage",
     HomePage : "HomePage",
+    DashboardPage: "DashboardPage",
+    Departments: "DepartmentsPage",
+    Teams: "TeamsPage",
+    Invitations: "InvitationsPage",
+    Messages: "MessagesPage",
+    Settings: "SettingsPage",
+    Payments: "PaymentsPage",
     ChooseOrg : "ChooseOrg",
     SignUpFromInvitationPage: "SignUpFromInvitation"
 }
