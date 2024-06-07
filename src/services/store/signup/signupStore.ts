4403ee9cfed3233f633acb2af847d000129c863e
olive.mengolo <mengolocharlie@gmail.com>
2024-06-07 19:01:14 +0100

    feat: Ajout des states pour l'initialisation du formulaire de signup

--- src/services/store/signup/signupStore.ts
+++ src/services/store/signup/signupStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { InvitationSignupStore, SignupStore } from "../../../utils/interfaces/type";
 import { InvitedUserSignupDatas, UserSignupData } from "../../api/DTO/request";
+import { CountryData, IndustryData } from "../../api/DTO/response";
 
 export const useSignupStore = create<SignupStore>((set) => ({
     signupStep: 1 as 1 | 2 | 3 | 4,
@@ -33,8 +34,24 @@ export const useSignupStore = create<SignupStore>((set) => ({
         orgLogo: null
     },
     setUserData: (values: UserSignupData) => set((state) => ({
-        userData: {...state.userData, ...values}
-    }))
+        userData: {
+            ...state.userData, 
+            ...values
+        }
+    })),
+    initCountryList: (countries: CountryData[]) => set({countryList: countries}),
+    countryList: [],
+    initIndustryList: (industries: IndustryData[]) => set({industryList: industries}),
+    industryList: [],
+    gender: [
+        { 
+            value: '0',
+            text: 'Homme'
+        },
+        { 
+            value: '1',
+            text: 'Femme'
+        }]
 }))
 
 export const useInvitationSignupStore = create<InvitationSignupStore>((set) => ({
