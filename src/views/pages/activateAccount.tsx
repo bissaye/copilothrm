033108abfd6691f7e240e659b8bf8ea364ba05bf
olive.mengolo <mengolocharlie@gmail.com>
2024-06-11 14:51:02 +0100

    fix: edited content height on activation page

--- src/views/pages/activateAccount.tsx
+++ src/views/pages/activateAccount.tsx
@@ -15,22 +15,24 @@ export const ActivateAccount: React.FC = () => {
 
     return(
         <Fragment>
-            <div className="h-full lg:h-[415px] w-full flex flex-col justify-center items-center gap-16">
-                {/* Partie à ajouter au cas où le lien d'activation a expiré */}
-                
-                <div className="w-[500px] h-[300px] bg-white shadow-md flex flex-col justify-center items-center gap-5">
-                    <FontAwesomeIcon icon={faCheckCircle} className="text-primary text-6xl" />
-                    <p className="text-2xl px-10 font-medium text-center">{formatMessage({id:"account_activated"})}</p>
-                    <LinkButton
-                            text= {formatMessage({id:"sign_in_link"})}
-                            type='primary'
-                            className='font-bold'
-                            textSize={15}
-                            onClick={() => {navigateById(pageIds.SignInPage)}}
-                        />
+            <main>
+                <div className="h-full w-full flex flex-col justify-between items-center">
+                    {/* Partie à ajouter au cas où le lien d'activation a expiré */}
+                    
+                    <div className="w-[500px] h-[300px] bg-white shadow-md flex flex-col justify-center items-center gap-5 mt-s12">
+                        <FontAwesomeIcon icon={faCheckCircle} className="text-primary text-6xl" />
+                        <p className="text-2xl px-10 font-medium text-center">{formatMessage({id:"account_activated"})}</p>
+                        <LinkButton
+                                text= {formatMessage({id:"sign_in_link"})}
+                                type='primary'
+                                className='font-bold'
+                                textSize={15}
+                                onClick={() => {navigateById(pageIds.SignInPage)}}
+                            />
+                    </div>
+                <FooterLandingPage/>
                 </div>
-            </div>
-            <FooterLandingPage/>
+            </main>
         </Fragment>
     )
 }
